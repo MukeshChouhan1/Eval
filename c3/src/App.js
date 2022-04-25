615379c755f1017b7b14ef0fa1fdcf207995fb33
Mukesh Chouhan <95852087+Mukeshfw14586@users.noreply.github.com>
2022-04-25 17:26:57 +0530

    Add files via upload

---
+++ c3/src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import {Home} from "./components/Home"
+import { Navbar } from './components/Navbar';
+import { Admin } from './components/Admin';
+import { EmployeeList } from './components/EmployeeList';
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/admin" element={<Admin/>}/>
+      <Route path="/employees" element={<EmployeeList/>}/>
+    </Routes>
+    
+    </BrowserRouter>
+  );
+}
+
+export default App;
